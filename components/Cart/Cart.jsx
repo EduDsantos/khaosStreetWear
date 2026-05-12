7b08b619eb6a967d8a0628fd7d4fd05ff8b4fff6
Eduardo dos Santos <111947576+EduDsantos@users.noreply.github.com>
2026-05-11 22:09:51 -0300

    comitando
    1

--- components/Cart/Cart.jsx
+++ components/Cart/Cart.jsx
@@ -6,9 +6,27 @@ export default function Cart() {
 
 
     return (
-        <button type='button' className="carrinho">
-            <img src="../images/carrinho.png" alt="" />
-            <span className='carrinho-status'>1</span>
-        </button>
+        <>
+
+            <button type='button' className="carrinho">
+                <img src="../images/carrinho.png" alt="" />
+                <span className='carrinho-status'>1</span>
+            </button>
+
+            <div className="lista-container">
+                <option value="">
+                    <ul className="carrinho-lista">
+                        <li>Produto 1</li>
+                        <li>Produto 2</li>
+                        <li>Produto 3</li>
+                        <li>Produto 3</li>
+                        <li>Produto 3</li>
+                        <li>Produto 3</li>
+                        <li>Produto 3</li>
+                        <li>Produto 3</li>
+                    </ul>
+                </option>
+            </div>
+        </>
     )
 }
